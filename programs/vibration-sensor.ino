37519229340efd4cf600bf222a2395d3bee01791
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2023-04-13 00:25:54 +0530

    Update vibration-sensor.ino

--- programs/vibration-sensor.ino
+++ programs/vibration-sensor.ino
@@ -1,22 +1,23 @@
-int Dig_pin = 7;
-int Ana_pin = A0;
-int Dig_out = LOW;
+int Dpin = 7;
+int Apin = A0;
+int Dout = LOW;
 int Ana_out = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(Dig_pin, INPUT);
   pinMode(Ana_pin, INPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Dig_out = digitalRead(Dig_pin);
-  Ana_out = analogRead(Ana_pin);
+  Dout = digitalRead(Dpin);
+  Aout = analogRead(Apin);
   Serial.print("Analog: ");
-  Serial.print(Ana_out);
+  Serial.print(Aout);
   Serial.print("  Digital: ");
-  Serial.print(Dig_out);
+  Serial.print(Dout);
   if(Ana_out <= 500){
     digitalWrite(13,HIGH);
     Serial.println("Vibration Detected!!!!!");
